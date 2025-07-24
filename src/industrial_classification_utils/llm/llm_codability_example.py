e251ef87dc1109d10ba87ecb1594ebce04c62bc7
Jyldyz D <j.djumalieva@gmail.com>
2025-07-24 11:21:54 +0000

    chore(llm): fix black formatting issue

--- src/industrial_classification_utils/llm/llm_codability_example.py
+++ src/industrial_classification_utils/llm/llm_codability_example.py
@@ -275,7 +275,7 @@ EXAMPLE_EMBED_SHORT_LIST = [
     },
     {
         "distance": 0.8145434260368347,
-        "title": "Counselling services for adults children and young people training" 
+        "title": "Counselling services for adults children and young people training"
         "for those working with children",
         "code": "88990",
         "four_digit_code": "8899",
