0ca757f564557061134120557104f6744781a7eb
peter-spencer-ons <peter.spencer@ext.ons.gov.uk>
2025-07-31 15:20:20 +0000

    test(llm): create vector store

--- src/industrial_classification_utils/llm/llm_forumlate_questions.py
+++ src/industrial_classification_utils/llm/llm_forumlate_questions.py
@@ -6,6 +6,7 @@ from industrial_classification_utils.embed.embedding import EmbeddingHandler
 from industrial_classification_utils.llm.llm import ClassificationLLM
 
 embed = EmbeddingHandler()
+embed.embed_index(from_empty=True)
 uni_chat = ClassificationLLM(model_name="gemini-2.0-flash", verbose=True)
 
 # Inputs for ClassificationLLM methods
