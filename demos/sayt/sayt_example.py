018da047e523e7ab0babff110c4a11d25b954db0
ivyONS <26869198+ivyONS@users.noreply.github.com>
2026-04-20 11:54:39 +0100

    add small example

---
+++ demos/sayt/sayt_example.py
@@ -0,0 +1,32 @@
+"""Run small example for industry/organisation descriptions SAYT."""
+
+# pylint: disable=protected-access, R0801
+
+# %%
+from industrial_classification_utils.sayt.sayt import SAYTSuggester
+
+# %%
+############# toy example to verify SAYT works #############
+small_corpus = [
+    ("Car wash", "Car Wash"),
+    ("Car wash", "CAR WASH (duplicate)"),
+    ("Car waxing", "Car Waxing"),
+    ("Waxing car", "Car Waxing"),
+    ("Carpentry services", "Carpentry services"),
+    ("Dog grooming", "Dog grooming"),
+    ("Cat grooming", "Cat grooming"),
+    ("USed car sales", "Used car sales"),
+    ("Car rental", "Car rental"),
+    ("Car repair", "Car repair"),
+    ("Car servicing", "Car servicing"),
+]
+suggester = SAYTSuggester(small_corpus)
+
+# %%
+for q in ["car", "cars", "waxi", "grom", "wash", "duplicate", "auto"]:
+    print("searching for:", q)
+    print("prefix", "->", suggester._get_prefix_suggestions(q))
+    print("ngram", "->", suggester._get_ngram_suggestions(q))
+    print("semantic", "->", suggester._get_semantic_suggestions(q))
+    print("combined", "->", suggester.suggest(q))
+    print()
